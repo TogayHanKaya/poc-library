12992429aa557d809490992889cb5df55b8be9c5
Togay Han Kaya <tkaya@mobiquityinc.com>
2022-07-28 10:56:18 +0200

    Fix async provider

--- app/src/main/java/com/mob/till/librarypoc/NetworkLibrary.kt
+++ app/src/main/java/com/mob/till/librarypoc/NetworkLibrary.kt
@@ -1,26 +1,37 @@
 package com.mob.till.librarypoc
 
 import android.content.Context
+import android.util.Log
 import com.google.android.gms.net.CronetProviderInstaller
 import org.chromium.net.CronetEngine
 import org.chromium.net.UrlRequest
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 
-class NetworkLibrary() {
+const val LOGGER_TAG = "NetworkLibrary LOG :"
+
+class NetworkLibrary {
 
     fun makeRequest(context: Context) {
-        CronetProviderInstaller.installProvider(context)
-        val myBuilder = CronetEngine.Builder(context)
-        val cronetEngine: CronetEngine = myBuilder.build()
-        val executor: Executor = Executors.newSingleThreadExecutor()
-        val requestBuilder = cronetEngine.newUrlRequestBuilder(
-            "http://192.168.178.115:8080/ping",
-            MyUrlRequestCallback(),
-            executor
-        )
+        CronetProviderInstaller.installProvider(context).addOnCompleteListener {
+            if (it.isSuccessful) {
+                Log.i(LOGGER_TAG, "Successfully installed Play Services provider: $it")
+                val myBuilder = CronetEngine.Builder(context)
+                val cronetEngine: CronetEngine = myBuilder.build()
+                val executor: Executor = Executors.newSingleThreadExecutor()
+                val requestBuilder = cronetEngine.newUrlRequestBuilder(
+                    "http://192.168.178.115:8080/ping",
+                    MyUrlRequestCallback(),
+                    executor
+                )
+                val request: UrlRequest = requestBuilder.build()
+                request.start()
+            } else {
+                Log.w(LOGGER_TAG, "Unable to load Cronet from Play Services", it.exception)
+            }
+        }
 
-        val request: UrlRequest = requestBuilder.build()
-        request.start()
+
+
     }
 }
