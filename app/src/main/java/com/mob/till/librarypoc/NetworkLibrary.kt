72ad85bd482fa5689c32a117ff8efd319cb5a791
Togay Han Kaya <tkaya@mobiquityinc.com>
2022-07-28 10:39:11 +0200

    Fix provider

--- app/src/main/java/com/mob/till/librarypoc/NetworkLibrary.kt
+++ app/src/main/java/com/mob/till/librarypoc/NetworkLibrary.kt
@@ -1,25 +1,26 @@
 package com.mob.till.librarypoc
 
 import android.content.Context
+import com.google.android.gms.net.CronetProviderInstaller
 import org.chromium.net.CronetEngine
 import org.chromium.net.UrlRequest
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 
-class NetworkLibrary(context: Context) {
+class NetworkLibrary() {
 
-    val myBuilder = CronetEngine.Builder(context)
-    val cronetEngine: CronetEngine = myBuilder.build()
-    val executor: Executor = Executors.newSingleThreadExecutor()
-    val requestBuilder = cronetEngine.newUrlRequestBuilder(
-        "http://192.168.178.115:8080/ping",
-        MyUrlRequestCallback(),
-        executor
-    )
+    fun makeRequest(context: Context) {
+        CronetProviderInstaller.installProvider(context)
+        val myBuilder = CronetEngine.Builder(context)
+        val cronetEngine: CronetEngine = myBuilder.build()
+        val executor: Executor = Executors.newSingleThreadExecutor()
+        val requestBuilder = cronetEngine.newUrlRequestBuilder(
+            "http://192.168.178.115:8080/ping",
+            MyUrlRequestCallback(),
+            executor
+        )
 
-    val request: UrlRequest = requestBuilder.build()
-
-    fun makeRequest() {
+        val request: UrlRequest = requestBuilder.build()
         request.start()
     }
 }
