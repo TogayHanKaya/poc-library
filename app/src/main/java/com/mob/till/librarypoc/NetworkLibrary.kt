71c96411f18140d61ea5c3b11ad4fc816f255c18
Togay Han Kaya <tkaya@mobiquityinc.com>
2022-07-28 11:43:59 +0200

    Switch to JavaCronetProvider

--- app/src/main/java/com/mob/till/librarypoc/NetworkLibrary.kt
+++ app/src/main/java/com/mob/till/librarypoc/NetworkLibrary.kt
@@ -1,10 +1,9 @@
 package com.mob.till.librarypoc
 
 import android.content.Context
-import android.util.Log
-import com.google.android.gms.net.CronetProviderInstaller
 import org.chromium.net.CronetEngine
 import org.chromium.net.UrlRequest
+import org.chromium.net.impl.JavaCronetProvider
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 
@@ -13,25 +12,15 @@ const val LOGGER_TAG = "NetworkLibrary LOG :"
 class NetworkLibrary {
 
     fun makeRequest(context: Context) {
-        CronetProviderInstaller.installProvider(context).addOnCompleteListener {
-            if (it.isSuccessful) {
-                Log.i(LOGGER_TAG, "Successfully installed Play Services provider: $it")
-                val myBuilder = CronetEngine.Builder(context)
-                val cronetEngine: CronetEngine = myBuilder.build()
-                val executor: Executor = Executors.newSingleThreadExecutor()
-                val requestBuilder = cronetEngine.newUrlRequestBuilder(
-                    "http://192.168.178.115:8080/ping",
-                    MyUrlRequestCallback(),
-                    executor
-                )
-                val request: UrlRequest = requestBuilder.build()
-                request.start()
-            } else {
-                Log.w(LOGGER_TAG, "Unable to load Cronet from Play Services", it.exception)
-            }
-        }
-
-
-
+        val javaBuilder = JavaCronetProvider(context).createBuilder()
+        val cronetEngine: CronetEngine = javaBuilder.build()
+        val executor: Executor = Executors.newSingleThreadExecutor()
+        val requestBuilder = cronetEngine.newUrlRequestBuilder(
+            "http://192.168.178.115:8080/ping",
+            MyUrlRequestCallback(),
+            executor
+        )
+        val request: UrlRequest = requestBuilder.build()
+        request.start()
     }
 }
